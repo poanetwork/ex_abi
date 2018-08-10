a9444d0ecca075cbe0f85bc659037baebd660fbe
Alex Garibay <alex@alexgaribay.com>
2018-08-10 15:40:47 -0500

    Add support for tuple type for inputs and outputs

--- test/abi/function_selector_test.exs
+++ test/abi/function_selector_test.exs
@@ -1,4 +1,70 @@
 defmodule ABI.FunctionSelectorTest do
   use ExUnit.Case, async: true
   doctest ABI.FunctionSelector
+
+  import ABI.FunctionSelector
+
+  describe "parse_specification_type/1" do
+    test "with a tuple and components" do
+      type = %{
+        "name" => "",
+        "type" => "tuple",
+        "components" => [
+          %{
+            "name" => "",
+            "type" => "uint256[6]"
+          },
+          %{
+            "name" => "",
+            "type" => "bool"
+          },
+          %{
+            "name" => "",
+            "type" => "uint256[24]"
+          },
+          %{
+            "name" => "",
+            "type" => "bool[24]"
+          },
+          %{
+            "name" => "",
+            "type" => "uint256"
+          },
+          %{
+            "name" => "",
+            "type" => "uint256"
+          },
+          %{
+            "name" => "",
+            "type" => "uint256"
+          },
+          %{
+            "name" => "",
+            "type" => "uint256"
+          },
+          %{
+            "name" => "",
+            "type" => "string"
+          }
+        ]
+      }
+
+      expected = {
+        :tuple,
+        [
+         {:array, {:uint, 256}, 6},
+         :bool,
+         {:array, {:uint, 256}, 24},
+         {:array, :bool, 24},
+         {:uint, 256},
+         {:uint, 256},
+         {:uint, 256},
+         {:uint, 256},
+         :string
+        ]
+      }
+
+      assert parse_specification_type(type) == expected
+    end
+  end
 end
