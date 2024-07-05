4c4b72f22bff519f8f476a103683611d8d2742a8
steffenix <87598469+steffenix@users.noreply.github.com>
2024-07-05 12:20:37 +0100

    make keccak module configurable (#169)

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule ABI.Mixfile do
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:jason, "~> 1.4"},
-      {:ex_keccak, "~> 0.7.5"},
+      {:ex_keccak, "~> 0.7.5", optional: true},
       {:propcheck, "~> 1.4", only: [:test, :dev]}
     ]
   end
